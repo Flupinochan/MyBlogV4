42bce059c994b1dd4f36f26cbe5df5d9418b629e
Flupinochan <flupino@metalmental.net>
2026-04-26 16:56:18 +0900

    skill card, dialogのcolorをcurrentColorやborder-currentを利用するように修正

--- frontend/src/components/home/skill.types.ts
+++ frontend/src/components/home/skill.types.ts
@@ -3,8 +3,7 @@ import type { SvgComponent } from "astro/types";
 export interface SkillItem {
   icon: SvgComponent;
   title: string;
-  summary: string;
-  details: string;
-  color: string;
+  description: string;
   tooltips: string[];
+  textColor: string;
 }
